cfcf03801a978ebabf839628b1cd3bf62cd8e263
Seto Masaki <m171831@xdev04.edu.tut.ac.jp>
2020-12-01 16:51:12 +0900

    Chante the output result

--- s4/B171831/Hello.java
+++ s4/B171831/Hello.java
@@ -3,7 +3,8 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello..(B171831)");
+        System.out.println("HelloWorld");
     }
 }
+
 
