9d08f4412e5256d81e822c4662acd61a6e2a9f4a
Seto Masaki <m171831@xdev02.edu.tut.ac.jp>
2020-12-01 17:44:06 +0900

    change the output result

--- s4/B171831/Hello.java
+++ s4/B171831/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!!");
+        System.out.println("Hello_World!!!");
     }
 }
 
