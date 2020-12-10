93628b36b2ea474dc0a23f77f9008a1effee1e6e
Seto Masaki <m171831@xdev03.edu.tut.ac.jp>
2020-12-10 13:53:41 +0900

    chenge the output message.

--- s4/B171831/Hello.java
+++ s4/B171831/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello_World!!!");
+        System.out.println("Hello_World!!");
     }
 }
 
